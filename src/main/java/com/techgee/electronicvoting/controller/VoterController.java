6a36b5cd45ca39b35b0fba1b23b8665854305ee9
Sharath Kumar <sharathkumar.dommasandraanandreddy@slu.edu>
2022-11-29 16:15:24 -0600

    added service to list poll for voter

--- src/main/java/com/techgee/electronicvoting/controller/VoterController.java
+++ src/main/java/com/techgee/electronicvoting/controller/VoterController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.techgee.electronicvoting.resource.PollResource;
 import com.techgee.electronicvoting.resource.VoterResource;
 import com.techgee.electronicvoting.service.VoterService;
 import com.techgee.electronicvoting.shared.Parameters;
@@ -29,6 +30,11 @@ public class VoterController {
 	@PostMapping(value="add-voter/{pollId}")
 	public boolean addVoter(@PathVariable(value="pollId") Long pollId, @RequestBody VoterResource voterResource) {
 		return voterService.addVoter(voterResource, Parameters.builder().id(pollId).build());
+	}
+	
+	@GetMapping(value="list-poll/{loginId}")
+	public List<PollResource> listPollToCastVote(@PathVariable(value="loginId") Long loginId) {
+		return voterService.listPollToCastVote(Parameters.builder().id(loginId).build());
 	}
 
 }
--- src/main/java/com/techgee/electronicvoting/service/VoterService.java
+++ src/main/java/com/techgee/electronicvoting/service/VoterService.java
@@ -2,6 +2,7 @@ package com.techgee.electronicvoting.service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -12,12 +13,15 @@ import org.springframework.stereotype.Service;
 import com.techgee.electronicvoting.dao.LoginDao;
 import com.techgee.electronicvoting.dao.PartyNameDao;
 import com.techgee.electronicvoting.dao.PartyRoleDao;
+import com.techgee.electronicvoting.dao.PollDao;
 import com.techgee.electronicvoting.dao.PrPrRelationDao;
 import com.techgee.electronicvoting.exception.VotingException;
 import com.techgee.electronicvoting.model.Login;
 import com.techgee.electronicvoting.model.PartyName;
 import com.techgee.electronicvoting.model.PartyRole;
+import com.techgee.electronicvoting.model.Poll;
 import com.techgee.electronicvoting.model.PrPrRelation;
+import com.techgee.electronicvoting.resource.PollResource;
 import com.techgee.electronicvoting.resource.VoterResource;
 import com.techgee.electronicvoting.shared.Parameters;
 
@@ -35,6 +39,9 @@ public class VoterService {
 	
 	@Autowired
 	PrPrRelationDao prPrRelationDao;
+	
+	@Autowired
+	PollDao pollDao;
 	
 	
 	
@@ -57,6 +64,38 @@ public class VoterService {
 		}
 		return checkAndCreatePrPr(partyRole, parameters);
 	}
+	
+	public List<PollResource> listPollToCastVote(Parameters parameters) {
+		Login login = loginDao.getV1(parameters).orElseThrow(() -> new VotingException("Login User does not exist"));
+		PartyRole partyRoleVoter = partyRoleDao.getV1(new Parameters(login.getPartyId(), PartyRole.VOTER_ROLE_CD), PartyRoleDao.BY_PARTYID_AND_PARTYROLECD).orElseThrow(() -> new VotingException("There is no Poll to cast vote"));
+		PartyRole partyRoleUser = partyRoleDao.getV1(Parameters.builder().id(partyRoleVoter.getPartyId()).foreignKey(PartyRole.USER_ROLE_CD).build(), 
+				PartyRoleDao.BY_PARTYID_AND_PARTYROLECD).orElse(null);
+		List<PrPrRelation> prPrRelation = prPrRelationDao.list(Parameters.builder().id(partyRoleUser.getPartyRoleId()).
+				foreignKey(partyRoleVoter.getPartyRoleId()).parentParameters(new Parameters
+						(PrPrRelation.USER_VOTER)).build(), PrPrRelationDao.BY_ROLES_AND_ROLE_CD_ENDDATE_NULL);
+		Set<Long> pollIds = new HashSet<>();
+		for(PrPrRelation prPrRelat: prPrRelation) {
+			pollIds.add(prPrRelat.getPollId());
+		}
+		List<Poll> polls = pollDao.listIn(null, pollIds);
+		List<PollResource> pollResources = new ArrayList<>();
+		polls.forEach(poll -> {
+			pollResources.add(setPollResource(poll));
+		});
+		return pollResources;
+	}
+	
+	private PollResource setPollResource(Poll poll) {
+		PollResource pollResource = new PollResource();
+		pollResource.setTitle(poll.getTitle());
+		pollResource.setDescription(poll.getDescription());
+		pollResource.setStartDate(poll.getStartDate());
+		pollResource.setEndDate(poll.getEndDate());
+		pollResource.setPollId(poll.getPollId());
+		pollResource.setPrPrRelationId(poll.getPprId());
+		return pollResource;
+	}
+
 	
 	private VoterResource setVoterResource(PartyName partyName) {
 		VoterResource voterResource  = new VoterResource();
