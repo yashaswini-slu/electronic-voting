67bbedadab1fbabeea9c20d0723ed8d7d62d97e5
Yashaswini Shivalingaiah <yashaswini.shivalingaiah@slu.edu>
2022-11-29 14:12:16 -0500

    added service and controller to add voter to a poll by pollId

--- src/main/java/com/techgee/electronicvoting/controller/VoterController.java
+++ src/main/java/com/techgee/electronicvoting/controller/VoterController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,9 @@ public class VoterController {
 		return voterService.listVoters(Parameters.builder().id(loginId).build());
 	}
 	
+	@PostMapping(value="add-voter/{pollId}")
+	public boolean addVoter(@PathVariable(value="pollId") Long pollId, @RequestBody VoterResource voterResource) {
+		return voterService.addVoter(voterResource, Parameters.builder().id(pollId).build());
+	}
 
 }
--- src/main/java/com/techgee/electronicvoting/service/VoterService.java
+++ src/main/java/com/techgee/electronicvoting/service/VoterService.java
@@ -1,5 +1,6 @@
 package com.techgee.electronicvoting.service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -10,8 +11,13 @@ import org.springframework.stereotype.Service;
 
 import com.techgee.electronicvoting.dao.LoginDao;
 import com.techgee.electronicvoting.dao.PartyNameDao;
+import com.techgee.electronicvoting.dao.PartyRoleDao;
+import com.techgee.electronicvoting.dao.PrPrRelationDao;
+import com.techgee.electronicvoting.exception.VotingException;
 import com.techgee.electronicvoting.model.Login;
 import com.techgee.electronicvoting.model.PartyName;
+import com.techgee.electronicvoting.model.PartyRole;
+import com.techgee.electronicvoting.model.PrPrRelation;
 import com.techgee.electronicvoting.resource.VoterResource;
 import com.techgee.electronicvoting.shared.Parameters;
 
@@ -23,6 +29,13 @@ public class VoterService {
 	
 	@Autowired
 	PartyNameDao partyNameDao;
+	
+	@Autowired
+	PartyRoleDao partyRoleDao;
+	
+	@Autowired
+	PrPrRelationDao prPrRelationDao;
+	
 	
 	
 	public List<VoterResource> listVoters(Parameters parameter) {
@@ -37,12 +50,47 @@ public class VoterService {
 		return resources;
 	}
 	
+	public boolean addVoter(VoterResource voterResource, Parameters parameters) {
+		PartyRole partyRole = partyRoleDao.getV1(new Parameters(voterResource.getPartyId(), PartyRole.VOTER_ROLE_CD), PartyRoleDao.BY_PARTYID_AND_PARTYROLECD).orElse(null);
+		if(partyRole == null) {
+			partyRole = createPartyRole(new Parameters(voterResource.getPartyId(), PartyRole.VOTER_ROLE_CD));
+		}
+		return checkAndCreatePrPr(partyRole, parameters);
+	}
+	
 	private VoterResource setVoterResource(PartyName partyName) {
 		VoterResource voterResource  = new VoterResource();
 		voterResource.setFirstName(partyName.getFirstName());
 		voterResource.setLastName(partyName.getLastName());
 		voterResource.setPartyId(partyName.getPartyId());
 		return voterResource;
+	}
+	
+	private PartyRole createPartyRole(Parameters parameters) {
+		PartyRole partyRole = new PartyRole();
+		partyRole.setPartyId(parameters.getId());
+		partyRole.setPartyRoleCd(parameters.getForeignKey());
+		partyRole.setStartDate(LocalDate.now());
+		return partyRoleDao.create(partyRole, parameters);
+	}
+	
+	private boolean checkAndCreatePrPr(PartyRole partyRoleVoter, Parameters parameters) {
+		PartyRole partyRoleUser = partyRoleDao.getV1(Parameters.builder().id(partyRoleVoter.getPartyId()).foreignKey(PartyRole.USER_ROLE_CD).build(), 
+				PartyRoleDao.BY_PARTYID_AND_PARTYROLECD).orElse(null);
+		PrPrRelation prPrRelation = prPrRelationDao.getV1(Parameters.builder().id(partyRoleUser.getPartyRoleId()).
+				foreignKey(partyRoleVoter.getPartyRoleId()).parentParameters(new Parameters
+						(PrPrRelation.USER_VOTER, parameters.getId())).build(), PrPrRelationDao.BY_ROLES_AND_ROLE_CD_POLL_ID_ENDDATE_NULL).orElse(null);
+		if(prPrRelation != null) {
+			throw new VotingException("Users is already voter");
+		}
+		prPrRelation = new PrPrRelation();
+		prPrRelation.setPartyRoleId1(partyRoleUser.getPartyRoleId());
+		prPrRelation.setPartyRoleId2(partyRoleVoter.getPartyRoleId());
+		prPrRelation.setPrPrRelationCd(PrPrRelation.USER_VOTER);
+		prPrRelation.setStartDate(LocalDate.now());
+		prPrRelation.setPollId(parameters.getId());
+		prPrRelation = prPrRelationDao.create(prPrRelation, null);
+		return true;
 	}
 
 }
